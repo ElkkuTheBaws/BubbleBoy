582e3a5cde84a9b3df9ba3163f6178d44863729c
ElkkuTheBaws <58703912+ElkkuTheBaws@users.noreply.github.com>
2025-01-26 04:31:01 +0200

    Stuff

--- Scripts/Data/Order.gd
+++ Scripts/Data/Order.gd
@@ -16,8 +16,12 @@ var completed: bool = false
 #TODO: Fix this BIG TIME
 func check_order(ingredient_list) -> bool:
 	if len(ingredient_list) != len(requirements):
+		print("yesyes")
 		return false
 	# Sort the lists and compare
 	ingredient_list.sort()
 	requirements.sort()
+	print(ingredient_list)
+	print(requirements)
+	print(ingredient_list == requirements)
 	return ingredient_list == requirements
